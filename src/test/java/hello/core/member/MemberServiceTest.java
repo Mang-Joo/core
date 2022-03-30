0402e930e96ba8988140dc54b9eb07e7ff10ad7f
KimWanJoo <wjwan0915@gmail.com>
2022-03-30 22:16:05 +0900

    TestCode 작성

---
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,21 @@
+package hello.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join() {
+        //given 이런이런게 주어졌을 때
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
