cf3f7ab64160d3fa3fc17ca9dfefaf9a99a1a68a
KimWanJoo <wjwan0915@gmail.com>
2022-04-01 22:05:54 +0900

    AppConfig 생성 -> 관심사를 분리하여 적용

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,12 +3,15 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+
+        AppConfig appConfig = new AppConfig();
+
+        MemberService memberService = appConfig.memberService(); /* AppConfig에서 결정헌다. */
+//        MemberService memberService = new MemberServiceImpl(); 직접 생성.
 
         Member member = new Member(1L, "memberA", Grade.VIP);
 
