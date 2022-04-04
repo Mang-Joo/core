0e9ec91ea89a2f90a13121379470c7303eb8c520
KimWanJoo <wjwan0915@gmail.com>
2022-04-04 21:50:15 +0900

    AppConfig Spring Bean 등록

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,17 +3,24 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.boot.web.reactive.context.AnnotationConfigReactiveWebApplicationContext;
+import org.springframework.context.ApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-
-        MemberService memberService = appConfig.memberService(); /* AppConfig에서 결정헌다. */
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService(); /* AppConfig에서 결정헌다. */
 //        MemberService memberService = new MemberServiceImpl(); 직접 생성.
 
+        /*SpringBean에 집어 넣어서 관리해준다.*/
+        ApplicationContext applicationContext = new AnnotationConfigReactiveWebApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
+
+        memberService.join(member);
 
         Member findMember = memberService.findMember(1L);
 
