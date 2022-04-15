44a399e0f3731055c54778cdb479988d281e4311
KimWanJoo <wjwan0915@gmail.com>
2022-04-15 21:53:27 +0900

    인터페이스 InitalizingBean, DisposableBean

--- src/test/java/hello/core/lifecycle/NetworkClientTest.java
+++ src/test/java/hello/core/lifecycle/NetworkClientTest.java
@@ -1,13 +1,14 @@
 package hello.core.lifecycle;
 
-public class NetworkClientTest {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClientTest implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClientTest() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메세지");
     }
 
     public void setUrl(String url) {
@@ -26,5 +27,20 @@ public class NetworkClientTest {
     //서비스 종료 시 호출
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        /*의존관계 주입이 끝나면 실행*/
+        System.out.println("NetworkClientTest.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        /*종료될 때 실행*/
+        System.out.println("NetworkClientTest.destroy");
+        disconnect();
     }
 }
